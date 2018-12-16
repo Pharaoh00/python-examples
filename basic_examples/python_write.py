808a6195d7ef644ec6fe189d20b2629aae9ad947
Pharaoh <hamoncsl@hotmail.com>
2018-12-16 19:14:52 -0200

    Update python_write.py

--- basic_examples/python_write.py
+++ basic_examples/python_write.py
@@ -4,5 +4,5 @@ from sys import argv
 script, filename = argv
 # A more clean and modern Python 3 to write to file.
 with open(filename, "w") as f:
-    print("Writing to file".format(filename)) # As python 3+
+    print("Writing to file: ".format(filename)) # As python 3+
     f.write("Hello World")
